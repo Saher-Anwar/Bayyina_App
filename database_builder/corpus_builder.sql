9a605297f019b0afcbb5100a40a4cdb6f659ce9e
Saher <saher.ziauddin@dal.ca>
2025-04-03 05:15:29 -0300

    Removed unnecessary SQL statements from corpus builder

--- database_builder/corpus_builder.sql
+++ database_builder/corpus_builder.sql
@@ -1,19 +1,13 @@
 create table corpus(
     chapter INT NOT NULL,
     verse INT NOT NULL,
-    `character` INT NOT NULL,
+    word_num INT NOT NULL,
     token INT NOT NULL,
     word VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
     tag VARCHAR(255) NOT NULL,
     info VARCHAR(255) NOT NULL,
     PRIMARY KEY(chapter, verse, `character`, token)
 )
-
-alter table corpus change column `character` word_num INT NOT NULL
-
-select * from corpus;
-
-delete from corpus;
 
 SELECT 
     verse, 
